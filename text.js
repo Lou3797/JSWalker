fd2921f8e118a4e6628d522c7bfc90881eed53e9
Nicholas Roth <lou3797@gmail.com>
2017-11-28 00:10:37 -0600

    Continuous printing for text sorted into an array added.

--- text.js
+++ text.js
@@ -40,18 +40,42 @@ var textbox = {
     y : 454,
     lineWidth : 52, //Max chars for a single line
     lineHeight : 30, //How many pixels to jump down after a line
-    timerFast : 16, //Quick timer value
     timerNormal : 35, //Normal timer value
-    textToPrint : ["This text should never appear."], // Full text we want to print, should be an array of max length 4
+    //textToPrint : "This text should never appear.", // Full text we want to print, should be an array of max length 4
     printedText : "", // Section of the text printed so far
     typeTimer : 35, // Timer to know when to print a new letter
     typePosition : 0, //Type cursor position
-    formatTextToPrint : function (dt, text) {
-        var gc = gameWindow.context;
-        gc.font="24px Consolas";
-        gc.textAlign = "left";
-        gc.fillStyle = "#DDDDDD";
+    typeWordPos : 0, //Type cursor position in the current word
+    typeArrayPos : 0, //Type cursor position in the array
+    wordsArray : [], //The array for current dialogue
+    updateWordArray : function (str) {
+        this.wordsArray = str.split(' ');
+    },
+    drawText : function (gc) {
+        var yOffset = 0;
+        if(this.typeWordPos > this.wordsArray[this.typeArrayPos].length) {
+            if(this.wordsArray[this.typeArrayPos+1]) {
+                this.typeArrayPos++;
+                this.typeWordPos = 0;
+            }
+        }
+        this.printedText = "";
+        for(var i = 0; i < this.typeArrayPos; i++) {
+            if(this.wordsArray[i] === "#") {
+                console.log("Skipping Octothorpe")
+            } else {
+                this.printedText += (this.wordsArray[i] + " ")
+            }
+        }
+        this.printedText += this.wordsArray[this.typeArrayPos].substring(0,this.typeWordPos);
 
+
+
+
+
+
+
+        gc.fillText(this.printedText, this.x + 15, this.y + 30);
     },
     update : function (dt) {
         var gc = gameWindow.context;
@@ -61,19 +85,23 @@ var textbox = {
         //This horrible mess handles gradually typing text
         if(this.typing) {
             this.typeTimer -= dt; //Decrease timer
-            if (this.printedText.length !== this.textToPrint.length) {
+            if (this.typeArrayPos < this.wordsArray.length - 1) {
+                //console.log(this.typeArrayPos + " < " + this.wordsArray.length + " : Continuing");
                 this.typing = true;
             }
             if(this.typeTimer <= 0) { //Timer done, we need to print a new letter
                 this.typeTimer = this.timerNormal;
-                this.typePosition++; //Adjust position, use string.sub to get sub-string
-                this.printedText = this.textToPrint.substring(0,this.typePosition);
+                this.typeWordPos++;
+                console.log(this.typeWordPos)
             }
-            else if(this.printedText.length === this.textToPrint.length) {
+            else if(this.typeArrayPos >= this.wordsArray.length - 1) {
+                //console.log(this.typeArrayPos + " >= " + this.wordsArray.length + " : Stopping");
                 this.typing = false;
             }
         }
         gc.drawImage(images[0], this.x, this.y);
+        this.drawText(gc);
+
         if(this.printedText.length <= this.lineWidth) {
             //???
         }
@@ -111,7 +139,9 @@ function update() { //Handles both update and draw functions- this is probably a
     if(textbox.open) {
         textbox.update(dt);
     } else {
+        textbox.updateWordArray("Kiana! I think you're great, alright? # I just wanted to make sure that you knew that I think you're super fantastic and that I'm glad I know you because I don't know what I would do if I didn't.");
         textbox.open = true;
+        textbox.typing = true;
     }
 
 }
